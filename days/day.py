dbbe70e9cf9b3f9ff71642669998d5103252830b
Adam Michalski <aadam.michalski@gmail.com>
2021-12-01 11:05:51 +0000

    Initial commit with day 1

---
+++ days/day.py
@@ -0,0 +1,39 @@
+from typing import TextIO
+
+
+class Day:
+    def __init__(self, number, same_inputs_all=False,
+                 same_inputs_test=False, same_inputs_actual=False):
+        self.number = number
+        same_inputs_actual = same_inputs_actual or same_inputs_all
+        same_inputs_test = same_inputs_test or same_inputs_all
+        self.in1 = f"input/{number}_1.in"
+        self.in2 = self.in1 if same_inputs_actual else \
+            f"input/{number}_2.in"
+        self.in1_test = f"input/{number}_1_test.in"
+        self.in2_test = self.in1_test if same_inputs_test else \
+            f"input/{number}_2_test.in"
+
+    def part1(f: TextIO, self):
+        raise NotImplementedError
+
+    def part2(f: TextIO, self):
+        raise NotImplementedError
+
+    def solve(self, test=False):
+        if not test:
+            in1 = self.in1
+            in2 = self.in2
+        else:
+            in1 = self.in1_test
+            in2 = self.in2_test
+        print(f"Advent Of Code 2021, day {self.number}")
+        with open(in1) as f1:
+            ans_part1 = self.part1(f1)
+        print(f"Part 1: {ans_part1}")
+        with open(in2) as f2:
+            ans_part2 = self.part2(f2)
+        print(f"Part 2: {ans_part2}")
+
+    def __call__(self, test):
+        self.solve(test)
---
+++ days/day01/day01.py
@@ -0,0 +1,31 @@
+from typing import TextIO
+from ..day import Day
+from queue import Queue
+
+
+class Day01(Day):
+    def __init__(self):
+        super().__init__("01", same_inputs_all=True)
+
+    def increasing_sums(self, f, window_width):
+        ans = 0
+        prev = 0
+        last = Queue()
+        for _ in range(window_width):
+            num = int(f.readline())
+            prev += num
+            last.put(num)
+        for line in f:
+            num = int(line)
+            next = prev + num - last.get()
+            if next > prev:
+                ans += 1
+            prev = next
+            last.put(num)
+        return ans
+
+    def part1(self, f: TextIO):
+        return self.increasing_sums(f, 1)
+
+    def part2(self, f: TextIO):
+        return self.increasing_sums(f, 3)
